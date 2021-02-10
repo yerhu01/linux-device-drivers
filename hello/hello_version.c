70a9f707c65987f86dd1dd4c3acc488b87171412
Darian Wang <darianjupiter@gmail.com>
2021-02-10 15:14:48 -0800

    time info

--- hello/hello_version.c
+++ hello/hello_version.c
@@ -3,20 +3,25 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/utsname.h>
+#include <linux/time.h>
 
 static char *whom = "world";
 module_param(whom, charp, 0644);
 MODULE_PARM_DESC(whom, "Recipient of the hello message");
 
+static unsigned long time;
+
 static int __init hello_init(void)
 {
 	pr_alert("Hello %s. You are currently using Linux %s.\n", whom, init_uts_ns.name.release);
+	time = get_seconds();
+	
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_alert("Goodbye.\n");
+	pr_alert("Loaded for %lu seconds.\n", get_seconds() - time);
 }
 
 module_init(hello_init);
