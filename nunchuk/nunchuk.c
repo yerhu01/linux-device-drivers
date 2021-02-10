899e384a084539ae3417814436832a9992754d1e
Darian Wang <darianjupiter@gmail.com>
2021-02-10 15:22:40 -0800

    basic i2c nunchuk driver

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -4,4 +4,33 @@
 #include <linux/i2c.h>
 
 
+static int nunchuk_probe(struct i2c_client *client)
+{
+	pr_err("%s called\n", __func__);
+
+	return 0;
+}
+
+static int nunchuk_remove(struct i2c_client *client)
+{
+	pr_err("%s called\n", __func__);
+
+	return 0;
+}
+
+static const struct of_device_id nunchuk_dt_ids[] = {
+	{ .compatible = "nintendo,nunchuk" },
+	{ },
+};
+
+static struct i2c_driver nunchuk_driver = {
+	.driver = {
+		.name = "nunchuk",
+		.of_match_table = nunchuk_dt_ids,
+	},
+	.probe_new = nunchuk_probe,
+	.remove = nunchuk_remove,
+};
+
+module_i2c_driver(nunchuk_driver);
 MODULE_LICENSE("GPL");
