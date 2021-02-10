8f7e687d4350243f1a920227c11f90dcd372ba37
Darian Wang <darianjupiter@gmail.com>
2021-02-10 15:35:19 -0800

    Add proper input device registration information

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -36,6 +36,13 @@ static int nunchuk_probe(struct i2c_client *client)
 	if (!input)
 		return -ENOMEM;
 
+	input->name = "Wii Nunchuk";
+	input->id.bustype = BUS_I2C;
+
+	set_bit(EV_KEY, input->evbit);
+	set_bit(BTN_C, input->keybit);
+	set_bit(BTN_Z, input->keybit);
+
 	ret = input_register_device(input);
 
 	buf[0] = 0xf0;
