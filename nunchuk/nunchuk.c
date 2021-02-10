a067e9e868c7b2af0394461e272427c89cd06115
Darian Wang <darianjupiter@gmail.com>
2021-02-10 15:29:18 -0800

    Add i2c access to nunchuk driver

--- nunchuk/nunchuk.c
+++ nunchuk/nunchuk.c
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
@@ -6,7 +7,28 @@
 
 static int nunchuk_probe(struct i2c_client *client)
 {
-	pr_err("%s called\n", __func__);
+	u8 buf[2];
+	int ret;
+
+	buf[0] = 0xf0;
+	buf[1] = 0x55;
+	ret = i2c_master_send(client, buf, sizeof(buf));
+	if (ret != sizeof(buf)) {
+		dev_err(&client->dev, "i2c_master_send failed (%d)\n", ret);
+		return ret;
+	}
+
+	udelay(1000);
+
+	buf[0] = 0xfb;
+	buf[1] = 0x00;
+	ret = i2c_master_send(client, buf, sizeof(buf));
+	if (ret != sizeof(buf)) {
+		dev_err(&client->dev, "i2c_master_send failed (%d)\n", ret);
+		return ret;
+	}
+
+	dev_err(&client->dev, "%s called\n", __func__);
 
 	return 0;
 }
